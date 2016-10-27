9c24ca190b326c5d796a7885803c0eab13adf644
Derek Roberts <derek.roberts@gmail.com>
2016-10-27 11:55:01 -0700

    Using random SQL password, then destroying

--- config/entrypoint.sh
+++ config/entrypoint.sh
@@ -22,23 +22,29 @@ TARGET=${TARGET:-192.168.1.193}
 DEL_DUMPS=${DEL_DUMPS:-"no"}
 
 
+# Random SQL password
+#
+SQL_PW=$( cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 )
+
+
 # Configure oscar12.properties
 #
 sed -i \
   -e "s/^#*E2E_DIFF *=.*/E2E_DIFF = ${E2E_DIFF}/" \
   -e "s/^#*E2E_DIFF_DAYS *=.*/E2E_DIFF_DAYS = ${E2E_DIFF_DAYS}/" \
   -e "s/^#*E2E_URL *=.*/E2E_URL = http:\/\/${TARGET}:3001\/records\/create/" \
+  -e "s/^#*db_password *=.*/db_password = ${SQL_PW}/" \
 /usr/share/tomcat6/oscar12.properties
 
 
 # Start and configure MySQL, import database and load dumps
 #
 service mysql start
-mysqladmin -u root password superInsecure
-mysql --user=root --password=superInsecure -e 'drop database if exists oscar_12_1;'
+mysqladmin -u root password ${SQL_PW}
+mysql --user=root --password=${SQL_PW} -e 'drop database if exists oscar_12_1;'
 cd /oscar_db/
-./createdatabase_bc.sh root superInsecure oscar_12_1
-mysql --user=root --password=superInsecure -e 'insert into issue (code,description,role,update_date,sortOrderId) select icd9.icd9, icd9.description, "doctor", now(), '0' from icd9;' oscar_12_1
+./createdatabase_bc.sh root ${SQL_PW} oscar_12_1
+mysql --user=root --password=${SQL_PW} -e 'insert into issue (code,description,role,update_date,sortOrderId) select icd9.icd9, icd9.description, "doctor", now(), '0' from icd9;' oscar_12_1
 
 
 # Import database and dumps
@@ -48,7 +54,7 @@ find /import/ -name "*.sql" | \
   while read IN
   do
     echo 'Processing:' ${IN}
-    mysql --user=root --password=superInsecure oscar_12_1 < "${IN}"
+    mysql --user=root --password=${SQL_PW} oscar_12_1 < "${IN}"
     echo "$(date +%Y-%m-%d-%T) ${IN}" | sudo tee -a /import/import.log
     [ "${DEL_DUMPS}" != "yes" ]|| mv "${IN}" "${IN}"-imported$(date +%Y-%m-%d-%T)
   done
@@ -65,5 +71,5 @@ mkdir -p /tmp/tomcat6-tmp/
   -Dcatalina.base=/var/lib/tomcat6 -Dcatalina.home=/usr/share/tomcat6 \
   -Djava.io.tmpdir=/tmp/tomcat6-tmp org.apache.catalina.startup.Bootstrap start
 #
-mysql --user=root --password=superInsecure -e 'drop database oscar_12_1;'
+mysql --user=root --password=${SQL_PW} -e 'drop database oscar_12_1;'
 service mysql stop
